3328816c1b5c43a70481dc982a18b27ddd3d8e93
Agustin Flores <agustinflores1505@gmail.com>
2023-11-26 16:05:40 -0300

    cambios en rutas de cart

--- src/routes/cart-routes.js
+++ src/routes/cart-routes.js
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto'
 import { Router } from 'express'
 import fs from 'fs'
 
@@ -8,12 +9,23 @@ router.get('/:id', async (req, res) => {
   const data = await fs.promises.readFile(path, 'utf-8')
   const carts = JSON.parse(data)
   const id = req.params.id
-  const cartToFind = carts.find((item) => item.id === id)
+  const cartToFind = carts.find((item) => item.id == id)
   if (cartToFind === undefined || !cartToFind) {
     res.status(404).send({ message: 'Cart not found' })
   } else {
-    res.status(200).send({ cartProducts: carts.products })
+    res.status(200).send({ cart: cartToFind })
   }
+})
+
+router.post('/', async (req, res) => {
+  const data = await fs.promises.readFile(path, 'utf-8')
+  const carts = JSON.parse(data)
+  const id = randomUUID()
+  const newCart = req.body
+  newCart.id = id
+  carts.push(newCart)
+  await fs.promises.writeFile(path, JSON.stringify(carts, null, '\t'))
+  res.status(201).send({ message: 'Cart created', newCart: newCart })
 })
 
 router.post('/:cid/products/:pid', async (req, res) => {
@@ -28,19 +40,20 @@ router.post('/:cid/products/:pid', async (req, res) => {
     res.status(404).send({ message: 'Cart not found' })
   } else {
     const productIndex = carts[cartIndex].products.findIndex(
-      (item) => item.id === productIdToFind
+      (item) => item.id == productIdToFind
     )
     if (productIndex !== -1) {
-      productExists.quantity += productQuantityToAdd
-      carts[cartIndex].products[productIndex] = productExists
-      fs.writeFileSync(path, JSON.stringify(carts, null, 2))
+      carts[cartIndex].products[productIndex].quantity += productQuantityToAdd
+      await fs.promises.writeFile(path, JSON.stringify(carts, null, '\t'))
+      res.status(201).send({ message: 'Product quantity modified' })
     } else {
-      product = {
+      let product = {
         id: productIdToFind,
         quantity: productQuantityToAdd,
       }
       carts[cartIndex].products.push(product)
-      fs.writeFileSync(path, JSON.stringify(carts, null, 2))
+      await fs.promises.writeFile(path, JSON.stringify(carts, null, '\t'))
+      res.status(201).send({ message: 'New product added' })
     }
   }
 })
