21e936569222ddfb25e16c41be83f9b7184c8206
Agustin Flores <agustinflores1505@gmail.com>
2023-12-28 18:02:15 -0300

    se agrego vista de cart con los productos del respectivo cart buscado.

--- src/routes/views-router.js
+++ src/routes/views-router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import productModel from '../dao/models/product.model.js'
 import messageModel from '../dao/models/message.model.js'
+import cartModel from '../dao/models/cart.model.js'
 
 const router = new Router()
 
@@ -17,6 +18,17 @@ router.get('/realtimeproducts', async (req, res) => {
 router.get('/chat', async (req, res) => {
   const messages = await messageModel.find()
   res.render('chat', { messages })
+})
+
+router.get('/carts/:cid', async (req, res) => {
+  const cid = req.params.cid
+  const cart = await cartModel.findById(cid).populate('products.product')
+
+  if (!cart) {
+    return res.status(404).send('Cart not found')
+  }
+
+  res.render('cart', { cart: cart.toJSON() })
 })
 
 export default router
