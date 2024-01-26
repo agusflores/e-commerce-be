5d65788b592d95912ba6601d030fd46383c1e8e7
Agustin Flores <agustinflores1505@gmail.com>
2024-01-26 20:45:10 -0300

    se agrega property cart a user

--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose'
+import cartModel from './cart.model.js'
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true, max: 30 },
   lastName: { type: String, required: true, max: 30 },
   email: { type: String, required: true, max: 50 },
   age: { type: Number, required: true },
+  cart: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: cartModel,
+  },
   password: { type: String, required: true, max: 100 },
   role: { type: String, default: 'user' },
 })
